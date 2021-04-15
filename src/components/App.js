10805b9fff8c279a4ec771660fa802fbb41f324d
olivia_02 <robson.livi@gmail.com>
2021-04-15 14:13:16 -0400

    Got database intigration succesfully!!! (Woot!)

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import ProductList from 'components/ProductList'
 import 'css/index.css'
+import firebase from '../util/firebase'
 
 const App = () => {
-	const productArray = [
-		{
-			"id": 1,
-			"img": 'src/img/lily1.jpg',
-			"type": "Lily",
-			"price": 4.00,
-			"para": "Lillies are fresh garden flowers, often symbolic of spring and innocence. Add them to bouquets that need pops of colour, or a brighter touch.",
-			"colours": ["Yellow", "White", "Pink", "Calla"]
-		},
-		{
-			"id": 2,
-			"img": 'src/img/rose4.jpg',
-			"type": "Rose",
-			"price": 2.00,
-			"para": "Roses symbolize love and romance and come in a variety of colours, they add a classic air to any bouquet and arrangement.",
-			"colours": ["Red", "White", "Pink", "Yellow"]
-		},
-		{
-			"id": 3,
-			"img": 'src/img/mum1.jpg',
-			"type": "Chrysanthemum",
-			"price": 3.00,
-			"para": "Chrysanthemums (also called 'mums' for short) are part of the Chinese Four Gentlemen flowers, along with plum blossoms, orchids, and bamboo; representing the four seasons. Mums represent autumn, and are perfect for fall seasonal arrangements.",
-			"colours": ["Red", "White", "Pink", "Yellow"]
-		},
-		{
-			"id": 4,
-			"img": 'src/img/anemone1.jpg',
-			"type": "Anemone",
-			"price": 3.00,
-			"para": "Also called 'Windflowers', anemones have various meanings throughout history and various cultures. They're in the buttercup family and can be various colours, making them versatile for any occasion.",
-			"colours": ["red", "white", "blue"]
-		}
-	]
+
+  
+  const [flowerList, setFlowerList] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const db = firebase.firestore()
+
+    db.collection(`Flowers`).get().then(
+      (snapshot) => {
+        const flowerData = []
+        console.log(snapshot.docs)
+
+        snapshot.docs.forEach(doc => {
+          console.log(doc.data())
+          flowerData.push(doc.data())
+        })
+
+      setFlowerList(flowerData)
+      console.log(flowerData)
+      setLoading(false)
+    })
+  },[])
 
 return (
   <div>
@@ -127,9 +117,9 @@ return (
           </fieldset>
         </form>
 
-      <ProductList products={productArray} /> 
+      <ProductList products={flowerList} /> 
 
-      <nav aria-label="Pagination" className="pagination">m
+      <nav aria-label="Pagination" className="pagination">
           <p>1-6 of 23 products found</p>
           <ol className="pages">
             <li><a href="#" aria-label="Current Page, Page 1" aria-current="true">1</a></li>
@@ -141,8 +131,5 @@ return (
         </nav>
       </main>
     </div>
-)
-
-}
-
+)}
 export default App
---
+++ src/util/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/firestore"
+
+	firebase.initializeApp({
+		apiKey: "AIzaSyBCjuS_FZZVjtOlRHdPl8Gc06jMDKVu9SQ",
+		authDomain: "wddm-olivia02.firebaseapp.com",
+		projectId: "wddm-olivia02",
+		storageBucket: "wddm-olivia02.appspot.com",
+		messagingSenderId: "1042953094881",
+		appId: "1:1042953094881:web:984b819c2d2eea022699a0",
+		measurementId: "G-Y1N1EWM4HV"
+		});
+
+export default firebase
